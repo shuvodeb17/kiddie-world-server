b7aa7f9c5683aea6fabd01fdd4872c071d94846f
Shuvo Deb <shuvodeb17014@gmail.com>
2023-05-18 15:32:13 +0600

    use env variable

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express')
+require('dotenv').config()
 const cors = require('cors');
 const app = express()
 const port = process.env.PORT || 5000
@@ -11,7 +12,7 @@ app.use(express.json())
 
 
 
-const uri = "mongodb+srv://kiddieWorld:tCsQ7yN3SOyR84U0@cluster0.b0yctrm.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.b0yctrm.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
